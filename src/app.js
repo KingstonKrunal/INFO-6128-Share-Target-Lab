fa1a812d25f043fe067f164f7041b8cc6c3ba647
Krunal Shah <42894940+KingstonKrunal@users.noreply.github.com>
2022-01-27 20:25:54 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const parsedUrl = new URL(window.location);
-    // searchParams.get() will properly handle decoding the values.
-    document.getElementById('title').value = parsedUrl.searchParams.get('title');
-    document.getElementById('text').value = parsedUrl.searchParams.get('text');
-    document.getElementById('url').value = parsedUrl.searchParams.get('url');
-});
+const elements = {
+  h1: document.querySelector('h1'),
+  titleInput: document.querySelector('#title'), 
+  textInput: document.querySelector('#text'), 
+  urlInput: document.querySelector('#url'), 
+  shareBtn: document.querySelector('#btn') 
+}
+
+
+const onShare = async() => {
+  const title = elements.titleInput.value;
+  const text = elements.textInput.value;
+  const url = elements.urlInput.value;
+
+   try {
+     if(navigator.share){
+       await navigator.share({
+         title, text, url
+       });
+
+       console.log("Successful!", {title, text, url})
+     }
+   } catch (err) {
+     console.error("Unsuccessful!!", err); 
+   }
+}
+
+elements.shareBtn.addEventListener('click', onShare);
